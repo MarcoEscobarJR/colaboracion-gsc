ee67a2181397692c65e0560f2b20bb9b53344f1e
Jeremias <jerexxypunto@gmail.com>
2022-03-04 17:14:10 -0300

    envuelvo la paginacion de single en un section

--- single.php
+++ single.php
@@ -32,32 +32,33 @@ get_header();
 
                 <div class="tel open-sans">
                     <div class="tel-img">
-                        <img src="./img/item/telefono.svg" alt="teléfono ícono">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/item/telefono.svg" alt="teléfono ícono">
                     </div>
                     <a href="tel:56999999999" class="inherit tel-text"><span itemprop = "telephone" >+569 99999999</span></a>
                 </div>
 
                 <div class="mail open-sans">
                     <div class="mail-img" style="height: 30px; overflow: hidden;">
-                        <img src="./img/item/mail.svg" alt="mail ícono">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/item/mail.svg" alt="mail ícono">
                     </div>
                     <a href="mailto:contacto@dominio.cl" class="inherit mail-text"><span itemprop="email">contacto@dominio.cl</span></a>
                 </div>
 
             </section>
-			<?php
-				the_post_navigation(
-					array(
-						'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'auto-fuentes' ) . '</span> <span class="nav-title">%title</span>',
-						'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'auto-fuentes' ) . '</span> <span class="nav-title">%title</span>',
-					)
-				);
-			?>
+			<section class="texto-articulo">
+				<?php
+					the_post_navigation(
+						array(
+							'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'auto-fuentes' ) . '</span> <span class="nav-title">%title</span>',
+							'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'auto-fuentes' ) . '</span> <span class="nav-title">%title</span>',
+						)
+					);
+				?>
+			</section>
 		<?php 
 			endwhile; // End of the loop. ?>
 
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
