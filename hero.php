25c7ca66c200feb2bee32c310f48b47917d25296
Jeremias <jerexxypunto@gmail.com>
2022-03-04 17:18:33 -0300

    areglo hero.php

--- hero.php
+++ hero.php
@@ -3,22 +3,22 @@
                 <ul class="ul-slider">
                     <li>
                         <h1 class="text-slider">Automotríz Fuetes</h1>
-                        <img src="<?php echo get_template_directory_uri(); ?>/<?php echo $carpet; ?>/slider-1.jpg" alt="">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/<?php echo $carpet; ?>/slider-1.jpg" alt="">
                     </li>
 
                     <li>
                         <p class="text-slider">Lorem 2</p>
-                        <img src="<?php echo get_template_directory_uri(); ?>/<?php echo $carpet; ?>/slider-2.jpg" alt="">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/<?php echo $carpet; ?>/slider-2.jpg" alt="">
                     </li>
 
                     <li>
                         <p class="text-slider">Lorem 3</p>
-                        <img src="<?php echo get_template_directory_uri(); ?>/<?php echo $carpet; ?>/slider-2.jpg" alt="">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/<?php echo $carpet; ?>/slider-2.jpg" alt="">
                     </li>
 
                     <li>
                         <p class="text-slider">Lorem 4</p>
-                        <img src="<?php echo get_template_directory_uri(); ?>/<?php echo $carpet; ?>/slider-3.jpg" alt="">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/<?php echo $carpet; ?>/slider-3.jpg" alt="">
                     </li>
                 </ul>
             </section>
