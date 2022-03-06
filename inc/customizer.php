55ce60585fbee342199eab88429afa17aaaa8118
Jeremias <jerexxypunto@gmail.com>
2022-03-06 20:29:54 -0300

    añado elemenos al perzonalziador de wordpress

--- inc/customizer.php
+++ inc/customizer.php
@@ -15,6 +15,51 @@ function auto_fuentes_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 
+	// Añado el panel
+	$wp_customize->add_panel('contacto-panel',array(
+		'title' => 'Datos de contacto',
+		'description' => '',
+		'priority' => 10,
+		'capability' => 'edit_theme_options',
+		'theme_supports' => ''
+	));
+
+	// Añado section
+	$wp_customize->add_section('info_contacto',array(
+		'title' => 'Información de contacto',
+		'panel' => 'contacto-panel',
+		'priority' => 1,
+		'capability' => 'edit_theme_options'
+	));
+
+	// Añado settings
+	$wp_customize->add_setting('campo_texto', array(
+		'type' => 'theme_mod',
+		'capability' => 'edit_theme_options'
+	));
+	$wp_customize->add_setting('campo_numero', array(
+		'type' => 'theme_mod',
+		'capability' => 'edit_theme_options'
+	));
+
+	// Añado controls
+	$wp_customize->add_control('campo_texto', array(
+		'label' => 'Añade un correo eletronico',
+		'section' => 'info_contacto' ,
+		'priority' => 1,
+		'type' => 'mail'
+	));
+	$wp_customize->add_control('campo_numero', array(
+		'label' => 'Añade un numero telefonico',
+		'section' => 'info_contacto' ,
+		'priority' => 2,
+		'type' => 'number'
+	));
+	
+	
+
+
+
 	if ( isset( $wp_customize->selective_refresh ) ) {
 		$wp_customize->selective_refresh->add_partial(
 			'blogname',
